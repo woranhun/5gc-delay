1b8e4d63df20751f33eb28e4e31700fab4028acf
Bence Orosz <oroszbence0929@gmail.com>
2021-12-08 20:18:54 +0100

    EventHelix 49 50 added

--- delaycalculator/UE.py
+++ delaycalculator/UE.py
@@ -4,7 +4,8 @@ import pyshark.packet.packet
 class UE:
 
     def __init__(self, suci: int):
-        self.packets = {"37": list()}
+        self.packets = {"24": list(), "33": list(), "37": list(), "38": list(), "39": list(), "40": list(),
+                        "47": list(), "48": list(), "49": list(), "50": list()}
         self.suci = suci
         self.amf_delay = 0
         self.autn = None
@@ -14,35 +15,57 @@ class UE:
     def addMessage(self, packet: pyshark.packet.packet.Packet, evetHelixId: int) -> None:
         self.packets[evetHelixId] = packet
 
-    def displayTimeDeltas(self, p1: pyshark.packet.packet.Packet, p2: pyshark.packet.packet.Packet) -> float:
+    def calculateDeltaT(self, p1: pyshark.packet.packet.Packet, p2: pyshark.packet.packet.Packet) -> float:
         dt = p2.sniff_time - p1.sniff_time
         return dt.total_seconds()
 
     def Calculate_24_33(self) -> None:
-        dt = self.displayTimeDeltas(self.packets["24"],
-                                    self.packets["33"])
+        dt = self.calculateDeltaT(self.packets["24"][0],
+                                  self.packets["33"][0])
         self.amf_delay += dt
         print("SUCI: [{0}] - EventHelix 24 and 33 - delay: {1:0.6f} s".format(self.suci,
                                                                               dt))
 
     def Calculate_37_38(self) -> None:
-        dt = self.displayTimeDeltas(self.packets["37"][0],
-                                    self.packets["37"][1])
-        dt += self.displayTimeDeltas(self.packets["37"][1],
-                                     self.packets["38"])
+        dt = self.calculateDeltaT(self.packets["37"][0],
+                                  self.packets["37"][1])
+        dt += self.calculateDeltaT(self.packets["37"][1],
+                                   self.packets["38"][0])
         self.amf_delay += dt
         print("SUCI: [{0}] - EventHelix 37 and 38 - delay: {1:0.6f} s".format(self.suci,
                                                                               dt))
 
     def Calculate_39_40(self) -> None:
-        dt = self.displayTimeDeltas(self.packets["39"],
-                                    self.packets["40"])
+        dt = self.calculateDeltaT(self.packets["39"][0],
+                                  self.packets["40"][0])
         self.amf_delay += dt
         print("SUCI: [{0}] - EventHelix 39 and 40 - delay: {1:0.6f} s".format(self.suci,
+                                                                              dt))
+
+    def Calculate_47_48(self) -> None:
+        dt = self.calculateDeltaT(self.packets["47"][0],
+                                  self.packets["47"][1])
+
+        dt += self.calculateDeltaT(self.packets["47"][1],
+                                   self.packets["48"][0])
+        self.amf_delay += dt
+        print("SUCI: [{0}] - EventHelix 47 and 48 - delay: {1:0.6f} s".format(self.suci,
+                                                                              dt))
+
+    def Calculate_49_50(self) -> None:
+        dt = self.calculateDeltaT(self.packets["49"][0],
+                                  self.packets["49"][1])
+
+        dt += self.calculateDeltaT(self.packets["49"][1],
+                                   self.packets["50"][0])
+        self.amf_delay += dt
+        print("SUCI: [{0}] - EventHelix 49 and 50 - delay: {1:0.6f} s".format(self.suci,
                                                                               dt))
 
     def displayTotalDelay(self) -> None:
         self.Calculate_24_33()
         self.Calculate_37_38()
         self.Calculate_39_40()
+        self.Calculate_47_48()
+        self.Calculate_49_50()
         print("Total AMF delay for SUCI [{0}] - {1:0.6f} ".format(self.suci, self.amf_delay))
--- delaycalculator/__init__.py
+++ delaycalculator/__init__.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 import pyshark
 
@@ -13,8 +14,16 @@ class DelayCalculator:
             raise RuntimeError("Capture not found!")
         self.cap = pyshark.FileCapture(capture_path, display_filter="ngap || nas-5gs || (http2 && tcp.port==7777)")
         self.UEs = []
-        self.stream_ids = {}
+        self.stream_ids_33_37 = {}
+        self.stream_ids_47_48 = {}
+        self.stream_ids_49_50 = {}
         self.amf_total_delay = 0
+        # '/nudr-dr/v1/subscription-data/imsi-001010000000002/context-data/amf-3gpp-access'
+        self.eventHelix4748PathPattern = re.compile(
+            "^/nudr-dr/v1/subscription-data/imsi-[0-9]+/context-data/amf-3gpp-access$")
+        # '/nudm-sdm/v2/imsi-001010000000002/am-data'
+        self.eventHelix4950PathPattern = re.compile(
+            "^/nudm-sdm/v2/imsi-[0-9]+/am-data$")
 
     def toStreamKey(self, tcpId: int, http2Id: int) -> str:
         return "tcp:{0}-http2:{1}".format(tcpId, http2Id)
@@ -39,14 +48,14 @@ class DelayCalculator:
                             and hasattr(layer, 'nas_5gs_mm_message_type') \
                             and int(layer.nas_5gs_mm_message_type) == 65:  # Message type: Registration request (0x41)
                         ue = UE(int(layer.nas_5gs_mm_suci_supi_null_scheme))
-                        ue.packets["24"] = pkt
+                        ue.packets["24"].append(pkt)
                         self.UEs.append(ue)
                     elif hasattr(layer, 'gsm_a_dtap_autn') \
                             and hasattr(layer, 'nas_5gs_mm_message_type') \
                             and int(layer.nas_5gs_mm_message_type) == 86:  # Message type: Authentication request (0x56)
                         for ue in self.UEs:
                             if ue.autn['5gAuthData']['autn'] == str(layer.gsm_a_dtap_autn).replace(':', ''):
-                                ue.packets["38"] = pkt
+                                ue.packets["38"].append(pkt)
                                 ue.ran_ue_ngap_id = int(layer.ran_ue_ngap_id)
                                 ue.amf_ue_ngap_id = int(layer.amf_ue_ngap_id)
                     elif hasattr(layer, 'uplinknastransport_element') \
@@ -54,49 +63,95 @@ class DelayCalculator:
                             and int(layer.nas_5gs_mm_message_type) == 87:  # Message type: Authentication request (0x57)
                         for ue in self.UEs:
                             if ue.amf_ue_ngap_id == int(layer.amf_ue_ngap_id):
-                                ue.packets["39"] = pkt
+                                ue.packets["39"].append(pkt)
                     elif hasattr(layer, 'downlinknastransport_element') \
                             and hasattr(layer, 'nas_5gs_security_header_type') \
                             and int(layer.nas_5gs_security_header_type) == 3:
                         for ue in self.UEs:
                             if ue.amf_ue_ngap_id == int(layer.amf_ue_ngap_id):
-                                ue.packets["40"] = pkt
+                                ue.packets["40"].append(pkt)
 
             elif pkt.highest_layer == 'HTTP2' \
                     and hasattr(pkt.http2, 'header') \
                     and str(pkt.http2.header) == 'Header: :method: POST' \
                     and str(pkt.http2.headers_path) == '/nausf-auth/v1/ue-authentications':
-                self.stream_ids[self.toStreamKey(
+                self.stream_ids_33_37[self.toStreamKey(
                     int(pkt.tcp.stream), int(pkt.http2.streamid))] = [None, 1]  # header + data + rspheader + rspdata
             elif pkt.highest_layer == 'HTTP2' \
                     and hasattr(pkt.http2, 'streamid') \
                     and self.toStreamKey(int(pkt.tcp.stream),
-                                         int(pkt.http2.streamid)) in self.stream_ids.keys() \
+                                         int(pkt.http2.streamid)) in self.stream_ids_33_37.keys() \
                     and str(pkt.http2.DATA_LAYER) == 'data' \
-                    and self.stream_ids[self.toStreamKey(int(pkt.tcp.stream),
-                                                         int(pkt.http2.streamid))][1] == 1:
+                    and self.stream_ids_33_37[self.toStreamKey(int(pkt.tcp.stream),
+                                                               int(pkt.http2.streamid))][1] == 1:
                 jsonstr = bytes.fromhex(str(pkt.http2.data_data).replace(':', '')).decode("ASCII")
                 if 'supiOrSuci' in jsonstr:
                     suci = json.loads(jsonstr)[
                         'supiOrSuci']
-                    self.findUEBySUCI(int(suci.split('-')[-1])).packets["33"] = pkt
-                    self.stream_ids[self.toStreamKey(int(pkt.tcp.stream),
-                                                     int(pkt.http2.streamid))] = [suci, 2]
-                else:
-                    print("SUCI helyett: " + str(pkt.highest_layer.http2.json_value_string))
+                    self.findUEBySUCI(int(suci.split('-')[-1])).packets["33"].append(pkt)
+                    self.stream_ids_33_37[self.toStreamKey(int(pkt.tcp.stream),
+                                                           int(pkt.http2.streamid))] = [suci, 2]
             elif pkt.highest_layer == 'HTTP2' \
                     and hasattr(pkt.http2, 'streamid') \
                     and self.toStreamKey(int(pkt.tcp.stream),
-                                         int(pkt.http2.streamid)) in self.stream_ids.keys():
-                self.stream_ids[self.toStreamKey(int(pkt.tcp.stream),
-                                                 int(pkt.http2.streamid))][1] += 1
-                ue = self.findUEBySUCI(int(self.stream_ids[self.toStreamKey(int(pkt.tcp.stream),
-                                                                            int(pkt.http2.streamid))][0].split(
+                                         int(pkt.http2.streamid)) in self.stream_ids_33_37.keys():
+                self.stream_ids_33_37[self.toStreamKey(int(pkt.tcp.stream),
+                                                       int(pkt.http2.streamid))][1] += 1
+                ue = self.findUEBySUCI(int(self.stream_ids_33_37[self.toStreamKey(int(pkt.tcp.stream),
+                                                                                  int(pkt.http2.streamid))][0].split(
                     '-')[-1]))
                 ue.packets["37"].append(pkt)
-                if self.stream_ids[self.toStreamKey(int(pkt.tcp.stream),
-                                                    int(pkt.http2.streamid))][1] == 4:
+                if self.stream_ids_33_37[self.toStreamKey(int(pkt.tcp.stream),
+                                                          int(pkt.http2.streamid))][1] == 4:
                     ue.autn = json.loads(bytes.fromhex(str(pkt.http2.data_data).replace(':', '')).decode("ASCII"))
+            elif pkt.highest_layer == 'HTTP2' \
+                    and hasattr(pkt.http2, 'header') \
+                    and str(pkt.http2.header) == 'Header: :method: PUT' \
+                    and self.eventHelix4748PathPattern.match(str(pkt.http2.headers_path)):
+                imsi = str(pkt.http2.headers_path).split('/')[4].split('-')[1]
+                suci = int(imsi[len(imsi) - 10:])
+                ue = self.findUEBySUCI(suci)
+                ue.packets["47"].append(pkt)
+                self.stream_ids_47_48[self.toStreamKey(int(pkt.tcp.stream), int(pkt.http2.streamid))] = [suci, 1]
+            elif pkt.highest_layer == 'HTTP2' \
+                    and hasattr(pkt.http2, 'streamid') \
+                    and self.toStreamKey(int(pkt.tcp.stream),
+                                         int(pkt.http2.streamid)) in self.stream_ids_47_48.keys():
+                stream = self.stream_ids_47_48[self.toStreamKey(int(pkt.tcp.stream),
+                                                                int(pkt.http2.streamid))]
+                ue = self.findUEBySUCI(stream[0])
+                if stream[1] == 1:
+                    ue.packets["47"].append(pkt)
+                    self.stream_ids_47_48[self.toStreamKey(int(pkt.tcp.stream), int(pkt.http2.streamid))] = [stream[0],
+                                                                                                             2]
+                elif stream[1] == 2:
+                    ue.packets["48"].append(pkt)
+                    self.stream_ids_47_48[self.toStreamKey(int(pkt.tcp.stream), int(pkt.http2.streamid))] = [stream[0],
+                                                                                                             3]
+            elif pkt.highest_layer == 'HTTP2' \
+                    and hasattr(pkt.http2, 'header') \
+                    and str(pkt.http2.header) == 'Header: :method: GET' \
+                    and self.eventHelix4950PathPattern.match(str(pkt.http2.headers_path)):
+                imsi = str(pkt.http2.headers_path).split('/')[3].split('-')[1]
+                suci = int(imsi[len(imsi) - 10:])
+                ue = self.findUEBySUCI(suci)
+                ue.packets["49"].append(pkt)
+                self.stream_ids_49_50[self.toStreamKey(int(pkt.tcp.stream), int(pkt.http2.streamid))] = [suci, 1]
+            elif pkt.highest_layer == 'HTTP2' \
+                    and hasattr(pkt.http2, 'streamid') \
+                    and self.toStreamKey(int(pkt.tcp.stream),
+                                         int(pkt.http2.streamid)) in self.stream_ids_49_50.keys():
+                stream = self.stream_ids_49_50[self.toStreamKey(int(pkt.tcp.stream),
+                                                                int(pkt.http2.streamid))]
+                ue = self.findUEBySUCI(stream[0])
+                if stream[1] == 1:
+                    ue.packets["49"].append(pkt)
+                    self.stream_ids_49_50[self.toStreamKey(int(pkt.tcp.stream), int(pkt.http2.streamid))] = [stream[0],
+                                                                                                             2]
+                elif stream[1] == 2:
+                    ue.packets["50"].append(pkt)
+                    self.stream_ids_49_50[self.toStreamKey(int(pkt.tcp.stream), int(pkt.http2.streamid))] = [stream[0],
+                                                                                                             3]
 
         for ue in self.UEs:
             ue.displayTotalDelay()
